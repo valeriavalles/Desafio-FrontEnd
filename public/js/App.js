c3401c8adca95afcf9b1f00ac6f545c06a90aed2
valeriavalles <valeria.sv@outlook.es>
2018-04-16 00:51:55 -0500

    agregando slider

---
+++ public/js/App.js
@@ -0,0 +1,27 @@
+$('#slider-cont').slick({
+
+    // normal options...
+    infinite: false,
+    
+    // the magic
+    responsive: [{
+    
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        infinite: true
+      }
+    }, 
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        // dots: true
+    }
+    }, 
+    {
+      breakpoint: 300,
+      // settings: "unslick" // destroys slick
+    
+    }]
+  });
